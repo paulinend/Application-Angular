a7abcaf76ce478f9935baa2e96311435a324102e
Pauline Ndiaye <Ayshoox@macbook-pro-de-pauline.home>
2016-10-01 15:33:28 +0200

    integration du ng-repeat fonctionnel

--- js/controllers/memberCtrl.js
+++ js/controllers/memberCtrl.js
@@ -1,12 +1,11 @@
 myApp.controller('memberCtrl', function($scope,$http){
   $http.get('data.json').then(function(response){
-      $scope.data = response.data;
+      $scope.myData = response.data.users;
     },
       function(err) {
         console.log("Error");
     })
-    $scope.pageTitle ="FUUUUCK";
-    $scope.message = 'Everyone come and see how good I look!';
+
 });
 //
 
